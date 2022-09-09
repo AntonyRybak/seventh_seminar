9203012f5b6f7ec31347f541ed20317c11252a21
Anton Rybak <toxa-wow@mail.ru>
2022-09-09 04:32:47 +0300

    task47 done

---
+++ task47/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+double[,] array = new double[3, 4];
+ 
+Random random = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = random.NextDouble() * 10 -1;
+        Console.Write("{0,1:F2}   ", array[i, j]);
+    }
+    Console.WriteLine();
+}
